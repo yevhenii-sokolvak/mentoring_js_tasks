ac0e04aece68f61d0f8c6c8dee2f4f6ffd01e7d9
Yevhenii <evgensokolvak2012@gmail.com>
2026-06-17 07:01:26 +0300

    task 1. aded remove task function. add task function.

--- task 1/script.js
+++ task 1/script.js
@@ -11,7 +11,7 @@ const PRIORITIES = {
 
 class Task {
     constructor(description, priority = PRIORITIES.medium, status = STATUSES.new) {
-        this.id = Date.now();
+        this.id = Date.now() + Math.random();
         this.description = description;
         this.status = status;
         this.priority = priority;
@@ -29,30 +29,87 @@ class Task {
 let task = new Task("Вивчити Реакт", PRIORITIES.high);
 let task2 = new Task("Завершити проект", PRIORITIES.low, STATUSES.inProgress);
 
+// TASK MANAGER
+
 const TaskManager = {
     tasks: [task, task2],
 
-    renderTasks: function() {
+    updateTaskStatus: function(taskId, newStatus) {
+        let task = this.tasks.find(t => t.id === taskId);
+        if (task) {
+            task.updateStatus(newStatus);
+            this.renderTasks();
+        }
+    },
+
+    updateTaskPriority: function(taskId, newPriority) {
+        let task = this.tasks.find(t => t.id === taskId);
+        if (task) {
+            task.updatePriority(newPriority);
+            this.renderTasks();
+        }
+    },
+
+    addTask: function(description, priority, status) {
+        let newTask = new Task(description, priority, status);
+        this.tasks.push(newTask);
+        this.renderTasks();
+    },
+
+    deleteTask: function(taskId) {
+        this.tasks = this.tasks.filter(t => t.id !== taskId);
+        this.renderTasks();
+    },
+
+    renderTasks: function(tasks = this.tasks) {
     let taskList = document.querySelector(".task-list");
 
-    this.tasks.forEach((task, index) => {
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
         taskList.innerHTML += `
             <div class="task-item" task-id=${task.id}>
-                <p>${task.description}</p>
-                Статус: <select onchange="updateTaskStatus(${task.id}, this.value)">
-                    <option value="${STATUSES.new}" ${task.status === STATUSES.new ? "selected" : ""}>${STATUSES.new}</option>
-                    <option value="${STATUSES.inProgress}" ${task.status === STATUSES.inProgress ? "selected" : ""}>${STATUSES.inProgress}</option>
-                    <option value="${STATUSES.completed}" ${task.status === STATUSES.completed ? "selected" : ""}>${STATUSES.completed}</option>
-                </select>
-                Пріоритет: <select onchange="updateTaskPriority(${task.id}, this.value)">
-                    <option value="${PRIORITIES.high}" ${task.priority === PRIORITIES.high ? "selected" : ""}>${PRIORITIES.high}</option>
-                    <option value="${PRIORITIES.medium}" ${task.priority === PRIORITIES.medium ? "selected" : ""}>${PRIORITIES.medium}</option>
-                    <option value="${PRIORITIES.low}" ${task.priority === PRIORITIES.low ? "selected" : ""}>${PRIORITIES.low}</option>
-                </select>
+                <strong>${task.description}</strong>
+                <div class=task-item__controls>
+                    <div>
+                        Статус: <select onchange="TaskManager.updateTaskStatus(${task.id}, this.value)">
+                            <option value="${STATUSES.new}" ${task.status === STATUSES.new ? "selected" : ""}>${STATUSES.new}</option>
+                            <option value="${STATUSES.inProgress}" ${task.status === STATUSES.inProgress ? "selected" : ""}>${STATUSES.inProgress}</option>
+                            <option value="${STATUSES.completed}" ${task.status === STATUSES.completed ? "selected" : ""}>${STATUSES.completed}</option>
+                        </select>
+                    </div>
+                    <div>
+                        Пріоритет: <select onchange="TaskManager.updateTaskPriority(${task.id}, this.value)">
+                            <option value="${PRIORITIES.high}" ${task.priority === PRIORITIES.high ? "selected" : ""}>${PRIORITIES.high}</option>
+                            <option value="${PRIORITIES.medium}" ${task.priority === PRIORITIES.medium ? "selected" : ""}>${PRIORITIES.medium}</option>
+                            <option value="${PRIORITIES.low}" ${task.priority === PRIORITIES.low ? "selected" : ""}>${PRIORITIES.low}</option>
+                        </select>
+                    </div>
+                </div>
+                <button onclick="TaskManager.deleteTask(${task.id})">Видалити</button>
             </div>
         `;
     })
-}
+    }
 }
 
 TaskManager.renderTasks();
+
+// FORM HANDLER
+const taskForm = document.querySelector(".task-form");
+
+taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let description = taskForm.querySelector("input[name='description']").value.trim(),
+        priority = PRIORITIES[taskForm.querySelector("select[name='priority']").value],
+        status = STATUSES[taskForm.querySelector("select[name='status']").value];
+
+    if (!description) {
+        return;
+    }
+
+    TaskManager.addTask(description, priority, status);
+
+    TaskManager.renderTasks();
+});
