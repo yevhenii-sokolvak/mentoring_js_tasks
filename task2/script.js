5f0e245932e02ea45657720b6f741764385dcf96
Yevhenii <evgensokolvak2012@gmail.com>
2026-06-23 15:12:25 +0300

    feat: task2. added add transaction functionality. added styles.

--- task2/script.js
+++ task2/script.js
@@ -4,26 +4,10 @@ const CATEGORIES = {
   'витрата': ['Продукти', 'Транспорт', 'Житло', 'Комунальні', 'Розваги', 'Здоров’я', 'Освіта', 'Інше']
 };
 
-const UA_MONTHS = ['січень','лютий','березень','квітень','травень','червень','липень','серпень','вересень','жовтень','листопад','грудень'];
+const UA_MONTHS = ['січня','лютого','березня','квітня','травня','червня','липня','серпня','вересня','жовтня','листопада','грудня'];
 
-class Transaction {
+class Entry {
     constructor(amount, category, date, type) {
-        if (amount <= 0) {
-            throw new Error('Сума має бути більшою за нуль');
-        }
-
-        if (!category) {
-            throw new Error('Категорія не може бути порожньою');
-        }
-
-        if (!date) {
-            throw new Error('Дата є обов’язковою.');
-        }
-
-        if (!type) {
-            throw new Error('Тип транзакції не може бути порожнім');
-        }
-
         this.id = crypto.randomUUID();
         this.amount = amount;
         this.category = category;
@@ -45,20 +29,12 @@ class Transaction {
     updateCategory(newCategory) {
         let category = String(newCategory || '').trim();
     
-        if (!category) {
-            throw new Error('Категорія не може бути порожньою.');
-        }
-        
         this.category = category;
         return this;
     }
 
     updateDate(newDate) {
         let date = new Date(newDate);
-
-        if (!date.getTime()) {
-            throw new Error('Невірний формат дати.');
-        }
 
         this.date = date;
         return this;
@@ -67,17 +43,72 @@ class Transaction {
     updateType(newType) {
         let type = String(newType || '').trim().toLowerCase();
 
-        if (!type) {
-            throw new Error('Тип транзакції не може бути порожнім.');
-        }
-
         this.type = type;
         return this;
     }
 }
 
-const FinanceManager {
+const FinanceManager = {
     transactions: [],
 
-    
+    addTransactions(amount, category, date, type) {
+        this.transactions.push(new Entry(amount, category, date, type));
+    },
+
+    createTransactionElement(transaction) {
+        const li = document.createElement('li');
+        li.classList.add('finance-records__item');
+
+        Object.entries(transaction).forEach(([key, value]) => {
+            if (key !== 'id') {
+                if (key === 'date') {
+                    const date = new Date(value);
+                    const formattedDate = `${date.getDate()} ${UA_MONTHS[date.getMonth()]} ${date.getFullYear()}`;
+                    value = formattedDate;
+                }
+
+                const span = document.createElement('span');
+
+                span.classList.add(`finance-records__${key}`);
+                span.textContent = value;
+
+                li.append(span);
+            }
+        });
+
+        return li;
+    },
+
+    renderTransactions(transactions = this.transactions) {
+        let listElement = document.querySelector('#records');
+        listElement.textContent = "";
+
+        transactions.forEach(transaction => {
+            listElement.append(
+                this.createTransactionElement(transaction)
+            );
+        });
+    },
 }
+
+FinanceManager.addTransactions(1000, 'Зарплата', new Date('2024-01-15'), 'дохід');
+FinanceManager.addTransactions(200, 'Продукти', new Date('2024-01-16'), 'витрата');
+FinanceManager.renderTransactions();
+
+const financeForm = document.querySelector('.finance-form');
+
+financeForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const amount = parseFloat(document.querySelector('#amount').value),
+          category = document.querySelector('#category').value,
+          date = new Date(document.querySelector('#date').value),
+          type = document.querySelector('#type').value;
+
+    try {
+        FinanceManager.addTransactions(amount, category, date, type);
+        FinanceManager.renderTransactions();
+    } catch (error) {
+        console.error('Error adding transaction:', error);
+    }
+});
