be420db85396f1f4d181b0bd421210b59dcb1cda
Yevhenii <evgensokolvak2012@gmail.com>
2026-07-17 07:03:29 +0300

    feat: reactjs. task1. added tailwindcss

--- reactjs/task1/src/App.jsx
+++ reactjs/task1/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <section id="center">
+      <section id="center" className="text-red-500">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
