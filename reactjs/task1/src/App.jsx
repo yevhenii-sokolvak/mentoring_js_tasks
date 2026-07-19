1f4590e5b1f3ede540a4928774e95e085eab60d4
Yevhenii <evgensokolvak2012@gmail.com>
2026-07-19 08:01:16 +0300

    feat: reactjs. task2. added tasklist. added styles and functionality.

--- reactjs/task1/src/App.jsx
+++ reactjs/task1/src/App.jsx
@@ -1,19 +1,8 @@
-import Task from './components/Task';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
-    <div className="max-w-xl mx-auto mt-10 space-y-3 px-4">
-      <Task
-        title="Вивчити пропси"
-        description="Розібратись, як передавати дані у компоненти"
-        isCompleted={true}
-      />
-      <Task
-        title="Додати Tailwind"
-        description="Налаштувати стилізацію проекту"
-        isCompleted={false}
-      />
-    </div>
+    <TaskList />
   );
 }
 
